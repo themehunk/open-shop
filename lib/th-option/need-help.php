0a1516174d02d8c366b416d25477394ef1f2d47a
harshv251 <harshv251@gmail.com>
2021-10-27 13:48:40 +0530

    searchp added join group text removed

--- lib/th-option/need-help.php
+++ lib/th-option/need-help.php
@@ -1,5 +1,5 @@
 <div class="theme_link">
-    <h3><?php _e('Contact Support','open-shop'); ?><!-- <php echo $theme_config['plugin_title']; ?> --></h3>
+    <h3><?php _e('Contact Support','open-shop'); ?></h3>
     <p><?php _e('If you need any help you can contact to our support team','open-shop'); ?></p>
     <p>
     <a target="_blank" href="https://themehunk.com/contact-us/"><?php _e('Need help ?','open-shop'); ?></a>
@@ -7,7 +7,7 @@
 </div>
 
 <div class="theme_link">
-    <h3><?php _e('Create a child theme','open-shop'); ?><!-- <php echo $theme_config['plugin_title']; ?> --></h3>
+    <h3><?php _e('Create a child theme','open-shop'); ?></h3>
     <p><?php _e("Before modifying theme core files. You should create child theme to make those changes update proof. Please follow this link to create child theme.",'open-shop'); ?></p>
     <p>
     <a target="_blank" href="https://themehunk.com/docs/how-to-create-a-child-theme-using-themehunk-themes/">
@@ -28,7 +28,6 @@
 <div class="theme_link">
     <h3><?php _e('Join Group','open-shop'); ?><!-- <php echo $theme_config['plugin_title']; ?> --></h3>
     <p> <?php _e('Join the community of friendly ThemeHunk users. Get connected, share opinion, ask questions and help each other !','open-shop');?></p>    
-    <p><?php _e("Please Join Our Facebook Group",'open-shop'); ?></p>
 
     <a href="https://www.facebook.com/groups/themehunk" target="_blank" rel="external noreferrer noopener" class="facebook-gropup is-link"><span class="dashicon dashicons dashicons-facebook-alt"></span><span>
         <?php _e(' Join our Facebook Group','open-shop'); ?></span></a>
