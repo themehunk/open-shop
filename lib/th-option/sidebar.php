fe62e613c82c34362a0f1f66ec00b47767bd1ad4
iheeresh <heereshd@gmail.com>
2025-02-17 14:07:42 +0530

    theme option updated

--- lib/th-option/sidebar.php
+++ lib/th-option/sidebar.php
@@ -27,13 +27,6 @@
             <hr>
 
             <div class="section">
-                <h3><?php _e('Video Tutorials','open-shop'); ?></h3>
-                <p><?php _e('Want a guide? We have video tutorials to walk you through getting started.','open-shop'); ?> </p>
-                <a href="https://www.youtube.com/watch?v=JE7o0DkAYeM&list=PLe42R8TvpeK3ouP1WFa_w_iOLaFDyW69W" target="_blank" class="sidebar-link"><?php _e('Watch Videos','open-shop'); ?></a>
-            </div>
-            <hr>
-
-            <div class="section">
                 <h3><?php _e('Support','open-shop'); ?> </h3>
                 <p><?php _e('Have a question, we are happy to help! Get in touch with our support team.','open-shop'); ?></p>
                 <a href="https://themehunk.com/contact-us/" target="_blank" class="sidebar-link"><?php _e('Submit a Ticket','open-shop'); ?></a>
